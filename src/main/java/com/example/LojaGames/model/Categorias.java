0b91c3ef5278c670f84af5e13126c32191457dfa
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-14 14:30:29 -0300

    Começo do projeto, criando pacote model e classes categorias, Produto e usuario

---
+++ src/main/java/com/example/LojaGames/model/Categorias.java
@@ -0,0 +1,34 @@
+package com.example.LojaGames.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = ("tb_categorias"))
+public class Categorias {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo tipo é obrigatório")
+    @Size(max = 200, message = "O atributo tipo deve ter no maximo 200 caracteres")
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/com/example/LojaGames/model/Produto.java
@@ -0,0 +1,80 @@
+package com.example.LojaGames.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = ("tb_produtos"))
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotBlank(message = "O atributo nome é obrigatório")
+    @Size(max = 200, message = "O atributo nome deve ter no maximo 200 caracteres")
+    private String nome;
+
+    @Size(max = 500, message = "O atributo descrição deve ter no maximo 500 caracteres")
+    private String descricao;
+
+    @NotBlank(message = "O atributo quantidade é obrigatório")
+    private int quantidade;
+
+    @NotBlank(message = "O atributo preço é obrigatório")
+    private  float preco;
+
+    @Size(max = 5000, message = "O atributo foto deve ter no maximo 5000 caracteres")
+    private String foto;
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
+
