3404d4d16c55b2739ca6bbc1bf8ea25a05beca9b
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-17 14:13:59 -0300

    Adicionado endpoint delete no produtocontroller.

--- src/main/java/com/example/LojaGames/Controller/ProdutoController.java
+++ src/main/java/com/example/LojaGames/Controller/ProdutoController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -35,4 +37,20 @@ public class ProdutoController {
     public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
         return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
     }
+    @PutMapping
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+        return produtoRepository.findById(produto.getId()).map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                .body(produtoRepository.save(produto)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        Optional<Produto> produto = produtoRepository.findById(id);
+        if(produto.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        produtoRepository.deleteById(id);
+    }
+
 }
