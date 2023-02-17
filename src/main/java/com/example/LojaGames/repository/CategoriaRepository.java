61cc9ab667858a77f26f2b1030b067593e426693
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-17 14:40:12 -0300

    Adicionado endpoints na categoriaController.

--- src/main/java/com/example/LojaGames/repository/CategoriaRepository.java
+++ src/main/java/com/example/LojaGames/repository/CategoriaRepository.java
@@ -1,10 +1,14 @@
 package com.example.LojaGames.repository;
 
 import com.example.LojaGames.model.Categoria;
+import com.example.LojaGames.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+        public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String nome);
 }
